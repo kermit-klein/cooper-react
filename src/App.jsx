9e5d82dfcdb7ba45db4b85b8572be66e035c94c7
japh <jenny.scherr@svenskaspel.se>
2020-04-29 11:17:16 +0200

    added render login form if login button is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import DisplayCooperResult from "./components/DisplayCooperResult";
 import InputFields from "./components/InputFields";
+import LoginForm from "./components/LoginForm";
 
 class App extends Component {
   state = {
     distance: "",
     gender: "female",
     age: "",
+    renderLoginForm: false,
   };
 
   onChangeHandler = (e) => {
@@ -14,9 +16,20 @@ class App extends Component {
   };
 
   render() {
+    const renderLogin = this.state.renderLoginForm ? (
+      <LoginForm />
+    ) : (
+      <button
+        id="login"
+        onClick={() => this.setState({ renderLoginForm: true })}
+      >
+        Login
+      </button>
+    );
     return (
       <>
         <InputFields onChangeHandler={this.onChangeHandler} />
+        {renderLogin}
         <DisplayCooperResult
           distance={this.state.distance}
           gender={this.state.gender}
