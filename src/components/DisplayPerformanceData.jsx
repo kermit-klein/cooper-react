7648b9eff90dc2486ea060c78e8f8b7ede4df572
allezz <aerbay@gmail.com>
2020-05-02 11:36:15 +0200

    Add chart.js,Add Line diagram

--- src/components/DisplayPerformanceData.jsx
+++ src/components/DisplayPerformanceData.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { getData } from "../modules/performanceData";
+import { Line } from "react-chartjs-2";
+import { Container, Segment } from "semantic-ui-react";
 
 class DisplayPerformanceData extends Component {
   state = {
@@ -21,18 +23,55 @@ class DisplayPerformanceData extends Component {
     });
   }
   render() {
-    let dataIndex;
+    let receivedData = this.state.performanceData;
+    let [dataMessage, dataDistance] = [[], []];
 
-    if (this.state.performanceData != null) {
-      dataIndex = (
-        <div>
-          {this.state.performanceData.map((item) => {
-            return <div key={item.id}>{item.data.message}</div>;
-          })}
-        </div>
-      );
+    if (receivedData != null) {
+      receivedData.forEach((e) => {
+        dataMessage.push(e.data.message);
+        dataDistance.push(e.data.distance);
+      });
     }
-    return <div id="index">{dataIndex}</div>;
+    let chartData = {
+      labels: dataMessage,
+      datasets: [
+        {
+          data: dataDistance,
+          label: "Cooper Record",
+          fill: true,
+          lineTension: 0.3,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+        },
+      ],
+    };
+    return (
+      <Segment>
+        <Container>
+          <div id="index">
+            <Line
+              data={chartData}
+              width={50}
+              height={200}
+              options={{ maintainAspectRatio: false }}
+            />
+          </div>
+        </Container>
+      </Segment>
+    );
   }
 }
 export default DisplayPerformanceData;
