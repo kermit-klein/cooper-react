608ba26e60a040cec434b80cf7132eee1dbd4def
allezz <aerbay@gmail.com>
2020-05-02 15:54:41 +0200

    Add line-chart,radar-chart,pie-chart

--- src/components/DisplayPerformanceData.jsx
+++ src/components/DisplayPerformanceData.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { getData } from "../modules/performanceData";
-import { Line } from "react-chartjs-2";
-import { Container, Segment } from "semantic-ui-react";
+import { Line, Pie, Radar } from "react-chartjs-2";
+import { Container, Segment, Grid } from "semantic-ui-react";
 
 class DisplayPerformanceData extends Component {
   state = {
@@ -22,6 +22,7 @@ class DisplayPerformanceData extends Component {
       this.props.indexUpdated();
     });
   }
+
   render() {
     let receivedData = this.state.performanceData;
     let [dataMessage, dataDistance] = [[], []];
@@ -32,6 +33,28 @@ class DisplayPerformanceData extends Component {
         dataDistance.push(e.data.distance);
       });
     }
+
+    let occurance = (arr) => {
+      var a = [],
+        b = [],
+        prev;
+
+      arr.sort();
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] !== prev) {
+          a.push(arr[i]);
+          b.push(1);
+        } else {
+          b[b.length - 1]++;
+        }
+        prev = arr[i];
+      }
+
+      return [a, b];
+    };
+
+    let pieRadarOccurance = occurance(dataMessage);
+
     let chartData = {
       labels: dataMessage,
       datasets: [
@@ -58,6 +81,38 @@ class DisplayPerformanceData extends Component {
         },
       ],
     };
+
+    let pieChartData = {
+      labels: pieRadarOccurance[0],
+      datasets: [
+        {
+          data: pieRadarOccurance[1],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56",
+            "#449c44",
+            "#db822a",
+            "#bb2adb",
+          ],
+        },
+      ],
+    };
+    let radarChartData = {
+      labels: pieRadarOccurance[0],
+      datasets: [
+        {
+          data: pieRadarOccurance[1],
+          backgroundColor: "rgba(255,99,132,0.2)",
+          borderColor: "rgba(255,99,132,1)",
+          pointBackgroundColor: "rgba(255,99,132,1)",
+          pointBorderColor: "#fff",
+          pointHoverBackgroundColor: "#fff",
+          pointHoverBorderColor: "rgba(255,99,132,1)",
+          label: "Cooper Performance",
+        },
+      ],
+    };
     return (
       <Segment>
         <Container>
@@ -70,6 +125,27 @@ class DisplayPerformanceData extends Component {
             />
           </div>
         </Container>
+        <Grid divided="vertically">
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              {" "}
+              <Pie
+                data={pieChartData}
+                width={50}
+                height={200}
+                options={{ maintainAspectRatio: false }}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Radar
+                data={radarChartData}
+                width={50}
+                height={200}
+                options={{ maintainAspectRatio: false }}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Segment>
     );
   }
